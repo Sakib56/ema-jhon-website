dae419bb331ce2eb5ca48f5aab34bf34c2fda543
Sakib <sakib.hstu.bd@gmail.com>
2023-03-30 02:05:25 +0600

    show data from cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -5,16 +5,19 @@ const Cart = ({cart}) => {
     console.log(cart)
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-       totalPrice = totalPrice + product.price;
+    //    product.quantity = product.quantity || 1;
+       totalPrice = totalPrice + product.price * product.quantity;
        totalShipping = totalShipping + product.shipping;
+       quantity = quantity + product.quantity;
     }
     const totalTax = totalPrice*7/100;
     const grandTotal = totalPrice + totalShipping + totalTax;
     return (
         <div className='cart'>
             <h1>Order Summary</h1>
-            <p>Selected Item:{cart.length}</p>
+            <p>Selected Item:{quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${totalTax.toFixed(2)}</p>
